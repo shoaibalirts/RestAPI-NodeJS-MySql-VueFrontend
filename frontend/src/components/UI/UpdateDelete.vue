0cfd856b8ad8c3d16da6c54a71179482c0d84fea
shoaibalirts <shoaibali.rts@gmail.com>
2025-11-25 15:05:55 +0100

    added frontend developed in vue 3

---
+++ frontend/src/components/UI/UpdateDelete.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="d-flex justify-space-around ga-4">
+    <v-btn :icon="mdiPencil" @click="handleUpdate"></v-btn>
+    <v-btn :icon="mdiDelete" class="bg-primary" @click="handleDelete"></v-btn>
+  </div>
+</template>
+
+<script>
+import { mdiPencil, mdiDelete } from "@mdi/js";
+export default {
+  data() {
+    return {
+      mdiPencil,
+      mdiDelete,
+    };
+  },
+  methods: {
+    handleUpdate() {
+      console.log("update function is executing");
+    },
+    handleDelete() {
+      console.log("delete function is executing");
+    },
+  },
+};
+</script>
---
+++ frontend/src/components/layout/TheHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <the-search v-if="isSearch" />
+  <v-app-bar v-else :elevation="4">
+    <template v-slot:prepend>
+      <v-btn :icon="mdiMenu"></v-btn>
+    </template>
+
+    <v-app-bar-title @click="reloadApp">Dine Indkøbslister</v-app-bar-title>
+
+    <template v-slot:append>
+      <v-btn @click="callSearchComponent">
+        <v-icon :icon="mdiMagnify"></v-icon>
+      </v-btn>
+      <v-btn>
+        <v-icon :icon="mdiDotsVertical"></v-icon>
+      </v-btn>
+    </template>
+  </v-app-bar>
+</template>
+
+<script>
+import TheSearch from "../TheSearch.vue";
+import { mdiMagnify, mdiDotsVertical, mdiMenu } from "@mdi/js";
+export default {
+  data() {
+    return {
+      mdiMagnify,
+      mdiDotsVertical,
+      mdiMenu,
+      isSearch: false,
+    };
+  },
+  components: {
+    TheSearch,
+  },
+  methods: {
+    reloadApp() {
+      window.location.reload();
+    },
+    callSearchComponent() {
+      console.log("clicked");
+      this.isSearch = true;
+    },
+  },
+};
+</script>
