2f9370772ee215bb9fcfb09744d627dc748c8601
shoaibalirts <shoaibali.rts@gmail.com>
2025-11-27 13:05:22 +0100

    performing update functionality now

--- frontend/src/components/UI/TheMenu.vue
+++ frontend/src/components/UI/TheMenu.vue
@@ -9,7 +9,7 @@
 
   <v-list v-if="!isCookieData">
     <v-list-item v-for="(item, index) in items" :key="index" :to="item.to" router>
-      <v-list-item-title class="text-center bg-primary pa-4">
+      <v-list-item-title class="text-center bg-primary pa-2">
         {{ item.title }}
       </v-list-item-title>
     </v-list-item>
@@ -26,7 +26,7 @@ export default {
       items: [
         // { title: "Add a product", to: "/addproduct" },
         { title: "Produkter", to: "/products" },
-        // { title: "Find a single product", to: "/product" },
+        { title: "Find a single product", to: "/product" },
         // { title: "Update Product", to: "/updateproduct" },
         // { title: "Remove Product", to: "/deleteproduct" },
       ],
